84b97a28df6ca6fcdded7df59b7f0db6ae227e83
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-01 22:38:19 +0530

    Adding Reverse number

---
+++ Reverse Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reverse_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, reverse = 0, remainder;
+            Console.WriteLine("Enter a number");
+            n=int.Parse(Console.ReadLine());
+
+            while (n!=0)
+            {
+                remainder= n % 10;
+                reverse= reverse * 10 + remainder;
+                n/= remainder;
+            }
+            Console.WriteLine("{0} is a reversed number", reverse);
+        }
+    }
+}
