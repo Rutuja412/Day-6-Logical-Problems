cebaa5fb254a13263508c4ef05206f9f44c40de1
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-02 19:11:04 +0530

    Add Coupon number program

---
+++ Coupon Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Coupon_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a coupon number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                Random random = new Random();
+                string result = new string(Enumerable.Repeat(chars, 9).Select(s => s[random.Next(s.Length)]).ToArray());
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
