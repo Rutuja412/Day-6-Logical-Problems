bcbe2a881d7e09f87544b801ce441ec8bd0f65bc
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-02 18:46:41 +0530

    Add stopwatch program

---
+++ Lc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ StopwatchExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace StopwatchExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i + "\n");
+            }
+            watch.Stop();
+            Console.WriteLine($"Execution time :{watch.ElapsedMilliseconds} ms");
+        }
+    }
+}
